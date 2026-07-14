d9eca6fbdd232878f50e6f225ade33b8d305f800
cviaco-87 <cviaco@gmail.com>
2026-07-14 21:15:42 +0200

    Create sabloniUslovi.js

---
+++ src/sabloniUslovi.js
@@ -0,0 +1,156 @@
+// Predefinisani šabloni opštih uslova po grupi radova, preuzeti iz predmjera iz prakse
+// (nije autorsko djelo — opšti tehnički uslovi) i prevedeni na srpski jezik (ijekavica).
+// Reference na normative (GN-400-1, GN-601 itd.) zadržane kako se koriste u praksi u RS/BiH.
+// Koriste se kao POLAZNI tekst koji korisnik ubaci jednim klikom i po potrebi doradi —
+// nisu obavezni ni fiksni. Grupe koje nemaju šablon ovdje korisnik popunjava ručno ili
+// preko AI asistenta (dugme "AI predlog uslova").
+
+export const SABLONI_USLOVI = {
+  "Pripremno-završni radovi": `Za obezbijeđenje funkcionisanja objekta u periodu izgradnje novog objekta škole potrebno je ograditi tj. onemogućiti kretanje đaka u dio gde se izvode radovi.
+
+OGRADA IZMEĐU GRADILIŠTA I POSTOJEĆEG OBJEKTA ŠKOLE
+
+Metalna ograda između gradilišta i stare škole.
+
+Nabavka materijala, doprema i izrada metalne ograde visine 6,00m sa pletenom žicom + cerada sa okruglim čeličnim stubovima fi 150mm sa temeljem.`,
+  "Demontaže i rušenja": `RUŠENjA I DEMONTAŽE
+
+Izvođač radova dužan je provesti sve propisane mjere važećeg Zakona o bezbednosti i zdravlja na radu i zaštiti od požara i drugih pozitivnih uredbi i tehničkih propisa za rad u naseljenom i prometnom mjestu, da spreči stvaranje i širenje prašine, složi i očisti materijal koji će se koristiti na određenu deponiju unutar lokacije objekta a sav šut i nepotrebne otpatke odneti na  deponiju. Jediničnim cijenama obuhvaćena je sva stručna i pomoćna snaga, potreban alat, prevozna sredstva za rad, unutrašnji nadzor.
+
+Prije izvođenja pozicija demontaže sa investitorom dogovoriti način i mjesto odlaganja demontiranih elemenata, kao i  način i mjesto odvoženja i predaju investitoru, što treba kompletno obuhvatiti cijenom pozicije.
+
+Materijal prilikom rušenja ili demontaže klasifikovati po vrstama i dimenzijama, složiti na privremenu deponiju koju nadzorni organ odredi u okviru gradilišta.  Izvršiti selekciju upotrebljivog i neupotrebljivog materijala i izdvojiti dio materijala predviđenog za ponovno ugrađivanje, a preostali dio zapisnički predati Investitoru na upotrebu.
+
+Cenom pozicija rušenja obuhvatiti i čišćenje prostora, iznošenje i odlaganje šuta i otpada , utovar na vozilo i odvoz na  deponiju do 10 km udaljenosti.
+
+Gradilište poslije rušenja i odvoza neupotrebljivog materijala mora biti čisto, uredno i osposobljeno za izvođenje radova. Delovi objekta koji se ruše ili demontiraju moraju biti tačno prema projektu obeleženi, kako bi se izbegla nepotrebna rušenja ili demontaže dijelova objekta koji nisu predviđeni za rušenje.
+
+Cenama pozicija obuhvatiti  i sav vertikalni i horizontalni transport na gradilištu. Rušenje vršiti pažljivo da ne bi došlo do oštećenja  elemenata koji se zadržavaju. Sva oštećenja nastala nepažnjom izvođača idu na teret izvođača radova.`,
+  "Zemljani radovi": `ZAJEDNIČKI I OPŠTI USLOVI ZA ZEMLjANE RADOVE
+
+Zemljani radovi se moraju izvesti neposredno prije početka gradnje stručno i kvalitetno, a u svemu prema važećim tehničkim uslovima i standardima, kao i prema uputstvima iz elaborata o geotehničkim ispitivanjima i prema tehničkom opisu za konstrukciju a u svemu prema crtežima.`,
+  "Betonski i AB radovi": `NAPOMENA:
+Grupa radova obuhvata:
+- Monolitnu betonsku konstrukciju
+- Tesarske radove uz AB konstrukciju
+
+ZAJEDNIČKI I OPŠTI USLOVI ZA BETONSKE RADOVE: 
+Izvođač radova je pored opisa pojedinačnih stavki radova cijenama obuhvatio i sljedeće zajedničke uslove:
+
+1. Betonski radovi će biti izvedeni u svemu po projektu, statičkom proračunu i važećim pravilnicima. Izvođač je dužan da prije početka izvođenja radova uraditi "Projekat betona sa planom izrade i kontrole kvaliteta betona".
+
+2. Ovlašćeni nadzorni organ izvršiće prijem oplate sa aspekta dimenzija, osovina i visinskih kota i prijem armature sa aspekta broja, prečnika i položaja ugrađenih šipki. Za sigurnost oplate na deformisanje i rušenje odgovaraće izvođač radova. Betoniranje će se izvršiti tek kad ovlašćeni nadzorni organ konstatuje u građevinskom dnevniku da sa svog aspekta nema primjedbi.
+
+3. Zaštita svježe (nega betona) izbetoniranih konstrukcija kao i zaštita betona (od mraza ili velikih vrućina) u fazi očvršćavanja je sadržana u jediničnim cijenama.
+
+4. Skidanje oplate može se izvršiti samo po odobrenju odgovornog lica.
+
+5. Sve veze između dijelova monolitne AB konstrukcije i osnovne noseće čelične, odnosno montažne AB konstrukcije, ostvariti zavarivanjem armaturnih petlji za anker ploče ili ubušivanjem armature u montažne elemente.
+
+6. Cijene sadrže sve radne operacije, utroške materijala, oplatu, pomoćni alat i skele kao i ostale troškove i zaradu preduzeća.
+
+7. Važeći normativ utroška rada i materijala propisuju "Normativi i standardi rada u građevinarstvu - Visokogradnja" GN-400-1.
+
+8. Pod pojmom "betoniranje u glatkoj oplati" podrazumijeva se da se mora dobiti potpuno ravna i glatka površina betona preko koje se direktno mogu izvoditi molersko farbarski radovi.
+
+9. Radnu skelu  obračunati u poziciji.
+
+10. Radne skele-podupirači ispod međuspratnih konstrukcija mogu se uklanjati isključivo uz saglasnost projektanta konstrukcije i nadzornog organa a prema statičkom proračunu koji će izvođač dostaviti na saglsnost u slučaju prevremene demontaže istih.
+
+11. Važeći normativ utroška rada i materijala  propisuju  "Normativi i standardi rada u građevinarstvu - Visokogradnja" GN-601`,
+  "Zidarski radovi": `Zidarski radovi će biti izvedeni u svemu prema projektu i važećim standardima i pravilnicima, odobrenim crtežima, tehničkom opisu, tehničkim uslovima iz elaborata za građevinsku fiziku i građevinskim normama sa odgovarajućom stručnom radnom snagom, uz punu primjenu savremene mehanizacije namijenjene ovoj vrsti radova.
+
+Zidarski radovi se ne smiju izvoditi na temperaturama ispod 3°C, osim u slučaju da postoji odobrenje nadzornog organa da se rad nastavi uz određene mjere zaštite, da bi se osigurala minimalna temperatura od 4°C dok ne dođe do očvršćavanja maltera.
+
+Kod izvođenja  radova na temperaturama višim ili nižim u odnosu na propisane, potrebno je preduzeti mjere zaštite.
+
+U slučaju da se zidanje prekine zbog hladnoće svi zidovi se na mjestu prekida rada moraju zaštititi od kvašenja i smrzavanja pokrivanjem po cijeloj debljini zida oplatom od daske i sl. Ako se zidovi oštete od kvašenja i mraza zbog loše zaštite, onda se prilikom nastavljanja radova moraju oštećeni zidovi porušiti i ponovo ozidati o trošku izvođača radova.`,
+  "Pokrivački radovi": `Svi krovopokrivački radovi moraju biti precizno i stručno izvedeni. Izvođač mora u celosti ispoštovati ugradnju po slojevima i materijale i uslove date u opisu krovopokrivačkih radova.
+
+U cenu obuhvatiti nabavku, horizontalne i vertikalne transporte, ugradnju osnovnih i pomoćnih materijala, sva vezna sredstva i čišćenje poslije izvođenja radova kao i odvoz šuta.
+
+Zidovi i plafoni koji formiraju puteve evakuacije- protivpožarna otpornost  F120.
+
+Proizvod treba da je ispitan i obeležen u skladu sa pravilnikom („Sl.Glasnik „ br. 52/2015).
+
+Izvođač je u obavezi da obezbijedi odgovarajuću dokuementaciju u  vidu atesta u skladu sa SRPS U.J1.092 za vatrootpornost od  120 minuta.`,
+  "Izolaterski radovi": `Prije početka izvođenja izolaterskih radova mora se izvršiti provera ispravnosti već izvedenih građevinskih, zanatskih i drugih radova koji bi mogli uticati na kvalitet izolacije. Svi gradjevinski ili zanatski radovi koji predhode izolaterskim radovima ili mogu svojim izvođenjem oštetiti postavljenu izolaciju moraju se izvršiti prije ovih, i to prema predviđenom redosledu. Prije nanošenja izolacije, podloga mora biti brižljivo poravnata, očišćena i potpuno suva. Izolaciona zaštita ne smije se polagati na betonske podloge ako proces vezivanja nije završen. Priprema podloge mora biti izvršena u potpunosti, naročito da čišćenje bude detaljno, sve čestice prašine uklonjene, eventualne mrlje od masti, ulja, kiselina odstranjene hemijskim putem i isprane vodom. U vrijeme početka izvodjenja izolaterskih radova podloga mora biti suva.
+
+Svi premazi prilikom nanošenja hidroizolacije, moraju biti izvedeni sa potpunim prekrivanjem površina bez mjehurića, izvedeni prema standardima, uputstvu proizvođača, dobro spojen. Prilikom izrade hidroizolacije, moraju se efikasno izolovati svi prodori kroz zidove i podove i da se uspostave vodonepropusne veze sa drugim materijalima i drugim izvedenim građevinskim elementima sa kojima hidroizolacija dolazi u kontakt.
+
+Ovi radovi se mogu uraditi i prema detaljima izvodjača ukoliko ih investitor, glavni projektant i naručilac prihvate kao bolje rješenje. Shodno datoj alternativi, uslovljava se i posebna obaveza specijalizovanog izvodjača svih izolaterskih radova da izolaterski radovi moraju biti izvedeni u svemu prema ispravnim detaljima, u skladu sa važećim propisima, upustvima i ispravnim načinom rada tako da pojedini dijelovi i slojevi izolacije u potpunosti odgovaraju svojoj namjeni, kvalitetu i dugotrajnosti.
+
+Ugradnju traka, izvodjenje preklopa, način nanošenja i debljina namaza kao i broj slojeva obavezno izvesti prema projektu i opisu. Svi detalji koji se odnose na parootparivače, dilatacione trake, završetke holkela i njihove dimenzije, obavezno se rade prema detaljima i upustvima bez ikakvlh odstupanja. Pored zidova i drugih vertikalnih površina, hidroizolaciju podići po visini zida u svemu prema detaljima.`,
+  "Bravarski radovi": `Napomena: sve pozicije postojećih građevinskih elemenata koje se zadržavaju prije predviđene  intervencije reparacije  moraju se pregledati  na licu mjesta
+
+NAPOMENA: Ugradnja svih elemenata sistema mora biti u skladu sa preporukama i tipskim detaljima proizvođača sistema i prema izvođačkim detaljima koje mora izraditi izvođač,  a  odobriti  nadzorni organ i investitor.
+
+PROTIVPOŽARNA VRATA`,
+  "Građevinska stolarija": `Napomena: sve pozicije aluminarije (prozori i vrata, spoljašnji i unutrašnji) moraju se premeriti na licu mjesta nakon demontaže postojeće. SVE MERE PROVERITI NA LICU MESTA.
+
+Napomena: Ugradnja svih elemenata sistema mora biti u skladu sa preporukama i tipskim detaljima proizvođača sistema i prema izvođačkim detaljima koje mora izraditi izvođač,  a  odobriti  nadzorni organ i investitor.
+
+Napomena:
+*Dimenzije otvora su date  prizvodnim mjerama.
+*Sve mjere proveriti na licu mjesta i na osnovu izvedenih otvora   uraditi poziciju 
+*Pozicije izvesti  u svemu prema opštem opisu, predmeru radova, šemi, izvođačkim detaljima ili prema kataloškoj specifikaciji proizvođača sistema i u skladu sa dimenzijama otvora, važećim propisima i standardima.
+*Izvođač je dužan da sve detalje usaglasi i pošalje na komentar Projektantu, kao i da podnese odgovarajuće sigurnosne ateste i sertifikate o kvalitetu i ispunjavanju  uslova zadatih projektom /PP, TEHNOLOŠKI USLOVI.../. Sve pozicije prije izrade mora da odobri nadzorni organ.
+
+Unutrašnja vrata`,
+  "Limarski radovi": `Napomena: u jediničnu cenu pozicije uračunati: osnovni materijal, potkonstrukciju, pomoćni materijal, spojna i zaptivna sredstva.`,
+  "Kamenorezački radovi": `Kamen koji treba da se ugrađuje  mora biti isečen na komade koji odgovaraju mjerama i obliku stepeništa.
+
+Izrada mora biti precizna i tačno po detaljima. Ivice moraju biti oštre i zaobljene, prema tome kako je predviđeno crtežima, u oba slučaja moraju biti izrađene pravo-bez talasa. Obrađene vidne površine ploča-lica-moraju biti ravne i ujednačene po cijeloj površini.
+
+Radovima je obuhvaćeno: doterivanje ploča, polaganje na vlažni malter, zalivanje spojnica cementnim mlekom. Gotove površine moraju biti horizontalne/vertikalve, tj. vidne površine ploča moraju biti sve u istoj ravni.
+
+Napomena 1.:
+
+1. U skladu sa  pravilnikom izvođač radova mora na svim stepeništima, da izvede gazišta koja moraju biti blago zakošena.
+
+2. Površine čela i rub stepeništa mora biti u kontarastu u odnosu na gazište, te oblaganje unutrašnjeg stepeništa izvođač radova će izvesti u dva  tona mermera:  svetliji i tamniji ton.
+
+3. Oblaganje  unutrašnjeg stepeništa i podesta izvođač radova će izvesti u dva  tona materijal:  svetliji i tamniji ton.`,
+  "Molersko-farbarski radovi": `Napomena: Radovi moraju biti povereni samo priznatom stručnom izvođaču. Svi radovi moraju biti izvedeni po mjerama i  crtežima iz projekta i projekta enterijera. Sve mjere izvođač uzima na licu mjesta prije početka izvođenja radova. Prije pristupanja radovima izvođač je dužan da pregleda podlogu i upozori nadzornog organa na eventualne nedostatke. Stare premaze nepodesne za podlogu treba skinuti pranjem, struganjem ili sl. Svi materijali se nanose na suvu, čvrstu i čistu površinu. Prije ugradnje potrebno je odstraniti sve slabo vezane dijelove starih nanosa boje i glet mase a naročito masna zaprljanja.
+
+Molersko farbarski radovi se vrše ručno ili mašinski, premazivanjem ili prskanjem i vrši se više puta. Dobijeni ton mora da bude čist, apsolutno ujednačen, da dobro pokriva podlogu i da bude bez tragova četke ili valjka. Svi završeci obojenih površina moraju da budu pravilni, boja ne smije da se ljušti, ljuska ili otire. Ukoliko projektant to traži, izvođač  je dužan da izvede uzorak minimalne površine 0,30m.
+Pri radu sa disperzivnim bojama se zabranjuje rad na temperaturi nižoj od +8 S, kao i bojenje  površina koje su direktno izložene jakim sunčevim zracima tokom letnjih meseci, ako nije drugačije napisano u tehničkom listu proizvođača boja.
+Svi materijali se nanose prema uputstvu proizvođača.`,
+  "Fasaderski radovi": `Opšti opis:
+
+Svi fasaderski radovi imaju se izvesti sa odgovarajućom stručnom radnom snagom, uz punu primjenu  savremenog alata i mehanizacije namijenjene ovoj vrsti radova.
+
+Svi upotrebljeni materijali, spojna, zaštitna i vezivna sredstva moraju biti propisanog kvaliteta, odnosno da poseduju ateste.
+
+Radovi se moraju izvesti kvalitetno, u svemu prema  važećim propisima, standardima i tehničkoj  dokumentaciji.  Podloga mora biti postojana, čista, suva i potpuno ravna. Prije nanošenja završnog sloja podlogu pripremiti u svemu prema važećim propisima i uputstvima proizvođača materijala. Pokrivni premazi moraju potpuno da pokriju podlogu. Kod površina gde se podloga posebno ne priprema izvršiti gitovanje manjih neravnina. Upotrebljeni materijali moraju dobro da prijanjaju, da su, prema svojoj namjeni, otporni, da nisu štetni po zdravlje da n edeluju agresivno na materijale  sa  kojima su u dodiru, da obrađene površine imaju oštre dodirne ivice.
+
+Kod temperatura nižih ili viših  od  propisanih, ukoliko se radovi izvode, preduzeti potrebne mjere zaštite upotrebljenog materijala. Mjere zaštite moraju trajati do god postoji potreba za istim. Mjere zaštite ne utiču na već ugovorenu cenu radova.
+
+Za sve vrijeme izvođenja odnosno do predaje objekta, izvođač je dužan da preduzme sve potrebne mjere, kako  ne bi  došlo do oštećanja ovih radova. Ako ipak i dođe do  oštećanja  ovih radova izvođač će o svom trošku, uz  saglasnost  nadzornog organa, radove dovesti u projektovano stanje. Prilikom izvođenja svojih radova, izvođač je dužan da ostale vrste radova čuva i sačuva od oštećenja.`,
+  "Gipsarski radovi": `MONTAŽNE PREGRADE i OBLOGE
+
+Napomena: Radovi moraju biti povereni samo priznatom stručnom izvođaču. Svi radovi moraju biti izvedeni po mjerama i  crtežima iz projekta. Sve mjere izvođač uzima na licu mjesta prije početka izvođenja radova.
+
+NAPOMENA:  Za stabilnost pregrada odgovara izvođač!
+U cenu ukalkulisati  radnu skelu, čeličnu podkonstrukciju, dodatna ojačanja za otvore; ojačanja  za slobodan kraj zida sa AL lajsnama; čelične dovratnike; nosače za konzolnu BC šolju; nosač za zidne baterije sa odvodom vode, nosače za tuš zidne baterije; univerzalne traverze;  izradu revizionih zidnih (standardnih i protivpožarnih) otvora sa prekrivnim zatvatačkim sistemom, eloksirano, s ugrađenom oblogom od Diamant ploče (dimenzije, položaj i broj revizionih otvora u svemu prema glavnom projektu instalacija).; Potrebna ojačanja  za staklene pregrade i ostale enterijerske elemente (po projektu enterijera police, plakare....); prosecanja za otvore kao  i bandažiranje i gletovanje spojeva i dr. što se neće posebno plaćati.
+
+U cenu ukalkulisati nanošenje temeljnog (disperzivnog) premaza - podloge za ujednačavanje upijanja podloge gipsanih ploča, učvršćivanje i skupljanje građevinske prašine sa podloge prije polaganje hidroizolacije i keramičkih pločica, boje...Disperzvni premaz tipa kao Knauf Tiefengrund ili odgovarajuće.
+
+Za potrebe ponude, neophodno je da izvođač dostavi ateste za predložene materijale.
+
+NAPOMENA:  Za stabilnost plafona odgovara izvođač !
+U cenu ukalkulisati  radnu skelu, čeličnu podkonstrukciju, dodatna ojačanja za otvore; ojačanja  za slobodan kraj plafona AL lajsnama; nosače za tuš plafonske baterije;   izradu revizionih (standardnih i protivpožarnih) plafonskih otvora sa prekrivnim zatvatačkim sistemom, eloksirano, s ugrađenom oblogom od Diamant ploče (dimenzije, položaj i broj revizionih otvora u svemu prema glavnom projektu instalacija); prosecanja za otvore kao  i bandažiranje i gletovanje spojeva i dr. što se neće posebno plaćati.`,
+  "Podopolagački radovi": `NAPOMENA: Kod velikih površina uraditi dilatacione spojnice, na svakih 36 m2, da ne dođe do pucanja u podu.
+
+Napomena: Radovi moraju biti povereni samo priznatom stručnom izvođaču. Svi radovi moraju biti izvedeni po mjerama i  crtežima iz projekta. Sve mjere izvođač uzima na licu mjesta prije početka izvođenja radova.
+
+POVRŠINE ZA KOMUNIKACIJU-HODNICI
+
+Radovi se moraju izvršiti stručno, kvalitetno i precizno u svemu prema tehničkim uslovima za izvođenje radova pri polaganju podnih obloga SRPS G.E5.021. Ukoliko za neke od materijala ne postoje standardi, proizvođač je dužan da atestom o kvalitetu potvrđuje sljedeće karakteristike: dimenzije, dimenzionalnu stabilnost, postojanost prema svetlosti, nezapaljivost, klizavost, elektroprovodljivost i ujednačene površine. Podloga za podne obloge mora biti kvalitetna i prilagođena za odgovarajuću vrstu podne obloge. Podloga mora biti tako izvedena da ispunjava sve uslove kvaliteta prema propisima. Temperatura vazduha u kojima se vrši oblaganje podova ne smije biti manja od +10ºC.
+
+Prilikom izrade podova poštovati tehnologiju rada prema uputstvima proizvođača materijala od pripreme podloge,  mehaničkim skidanjem oštećenih dijelova cementnih košuljicam tj.
+
+Prilikom postavljanje podnih obloga u dijelu komunikacija (hodnici) potrebno je da isti budu u klasi teško zapaljivih građevinskih materijala Bfl-s1 skladu sa SRPS EN 13501-1. Vatrootpornos po DIN 4102 je B1.
+
+Pripremni radovi`,
+}
